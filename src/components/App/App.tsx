0668b6af46df45ba42775d5e4575ca6b1d1e3e93
Can Zhao <zhaocan0928@gmail.com>
2023-10-30 20:55:41 +1100

    set the default page to inbox

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Inbox from "../Inbox/Inbox";
 import Files from "../Files/Files";
 import Navbar from "../Navbar/Navbar";
+import React from "react";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="*" element={<p>page not exist</p>} />
+        <Route path="/" element={<Inbox />} />
         <Route path="/inbox" element={<Inbox />} />
         <Route path="/files" element={<Files />} />
       </Routes>
